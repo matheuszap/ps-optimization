ec69c119c32a9c26c01dd929cdd0070539cd6567
zap <58528533+matheuszap@users.noreply.github.com>
2023-06-12 11:27:54 -0300

    .

--- pso.py
+++ pso.py
@@ -6,24 +6,33 @@ import matplotlib.pyplot as plt
 mean_fitness = []
 best_fitness = []
 
+def evaluate_griewank(position):
+    top1 = sum(x ** 2 for x in position)
+    top2 = math.prod(math.cos(p / math.sqrt(i + 1)) for i, p in enumerate(position))
+    top = (1 / 4000) * top1 - top2 + 1
+    return top
+
 class Particle:
     def __init__(self, dimensions, lower_bound, upper_bound):
-        self.position = [round(random.uniform(lower_bound, upper_bound), 3) for _ in range(dimensions)]
-        self.velocity = [round(random.uniform(0, 10), 3)] * dimensions
+        self.position = [random.uniform(lower_bound, upper_bound)  for _ in range(dimensions)]
+        self.velocity = [random.uniform(-5, 5)] * dimensions
         self.personal_best_position = self.position.copy()
         self.personal_best_fitness = float('inf')
 
-    def evaluate_griewank(self):
-        top1 = sum(x ** 2 for x in self.position)
-        top2 = math.prod(math.cos(p / math.sqrt(i + 1)) for i, p in enumerate(self.position))
-        top = (1 / 4000) * top1 - top2 + 1
+    def evaluate_fitness(self):
+        fitness = evaluate_griewank(self.position)
 
-        self.personal_best_fitness = round(top, 3)
+        # Atualiza o personal best (Partícula)
+        if fitness < self.personal_best_fitness:
+            self.personal_best_position = self.position.copy()
+            self.personal_best_fitness = fitness
+
+        return fitness
 
 # Parâmetros do PSO
 num_particles = 50
 num_dimensions = 10
-max_iterations = 1000
+max_iterations = 10000
 
 # Limites para as variáveis
 lower_bound = -100.0
@@ -42,36 +51,37 @@ c2 = 2.0  # Coeficiente de aprendizagem para a melhor posição global
 for _ in range(max_iterations):
     # Avaliação das Partículas
     for particle in particles:
-        particle.evaluate_griewank()
-        fitness = particle.personal_best_fitness
-
-        # Atualiza o personal best (Partícula)
-        if fitness < particle.personal_best_fitness:
-            particle.personal_best_position = particle.position.copy()
-            particle.personal_best_fitness = fitness
+        fitness = particle.evaluate_fitness()
 
         # Atualiza o global best (Enxame)
         if fitness < global_best_fitness:
             global_best_fitness = fitness
             global_best_position = particle.position.copy()
 
-        rand1 = random.uniform(0, 1)
-        rand2 = random.uniform(0, 1)
-
         for i in range(num_dimensions):
+            rand1 = random.uniform(0, 1)
+            rand2 = random.uniform(0, 1)   
             # Atualiza a velocidade da partícula
             new_velocity = (w * particle.velocity[i]) + (c1 * rand1 * (particle.personal_best_position[i] - particle.position[i])) + (c2 * rand2 * (global_best_position[i] - particle.position[i]))
-            particle.velocity[i] = round(new_velocity, 3)
+            particle.velocity[i] = new_velocity
 
             # Atualiza a posição da partícula
             new_position = particle.position[i] + particle.velocity[i]
-            particle.position[i] = round(new_position, 3)
+            # Verifica se a nova posição está fora dos limites
+            if new_position < lower_bound:
+                new_position = lower_bound
+            elif new_position > upper_bound:
+                new_position = upper_bound
+            particle.position[i] = new_position
 
-    print(global_best_fitness)
-    print(sum(particle.personal_best_fitness for particle in particles) / num_particles)
+    #print(global_best_fitness)
+    #print(sum(particle.personal_best_fitness for particle in particles) / num_particles)
     # Armazenar a média e o melhor global
     mean_fitness.append(sum(particle.personal_best_fitness for particle in particles) / num_particles)
     best_fitness.append(global_best_fitness)
+
+print(global_best_fitness)
+print(global_best_position)
 
 # Plotar o gráfico
 plt.plot(range(max_iterations), mean_fitness, label='Média do Enxame')
